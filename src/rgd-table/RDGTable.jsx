d1ee3e5480aa1d6ebc22c557b3a356b29ece1296
taphuong <taphuong@cmcglobal.vn>
2023-04-14 15:22:20 +0700

    update mock json

--- src/rgd-table/RDGTable.jsx
+++ src/rgd-table/RDGTable.jsx
@@ -4,6 +4,7 @@ import SelectFilter from "@inovua/reactdatagrid-community/SelectFilter";
 import "@inovua/reactdatagrid-enterprise/index.css";
 import Select from "react-select";
 import "./style.scss";
+import data from "./mock.json";
 
 const gridStyle = { minHeight: 550, marginTop: 10 };
 
@@ -24,62 +25,177 @@ const columns = [
     maxWidth: 40,
   },
   {
-    name: "firstName",
-    header: "First Name",
+    name: "salesDt",
+    header: "salesDt",
     locked: true,
     defaultVisible: true,
     textAlign: "center",
-    defaultFlex: 1,
   },
   {
-    name: "lastName",
-    header: "Last Name",
+    name: "suppCd",
+    header: "suppCd",
     textAlign: "center",
-    defaultFlex: 1,
   },
   {
-    name: "pictureId",
-    header: "PID",
+    name: "suppNm",
+    header: "suppNm",
     textAlign: "center",
-    render: (data) => {
-      return data?.value ? <div className="data-chip">{data?.value}</div> : "-";
-    },
   },
   {
-    name: "email",
-    header: "Email",
+    name: "purchCondCd",
+    header: "purchCondCd",
     textAlign: "center",
-    defaultFlex: 1,
   },
-
   {
-    name: "address",
-    header: "Address",
+    name: "purchCondNm",
+    header: "purchCondNm",
     textAlign: "center",
-    width: 800,
-    defaultFlex: 1,
-    render: ({ data }) => {
-      return data.address;
-    },
-    rowspan: ({ value = "False", dataSourceArray, rowIndex, column }) => {
+  },
+  {
+    name: "itemLclsCd",
+    header: "itemLclsCd",
+    textAlign: "center",
+  },
+  {
+    name: "itemLclsNm",
+    header: "itemLclsNm",
+    textAlign: "center",
+  },
+  {
+    name: "itemMclsCd",
+    header: "itemMclsCd",
+    textAlign: "center",
+  },
+  {
+    name: "itemMclsNm",
+    header: "itemMclsNm",
+    textAlign: "center",
+  },
+  {
+    name: "itemSclsCd",
+    header: "itemSclsCd",
+    textAlign: "center",
+  },
+  {
+    name: "itemSclsNm",
+    header: "itemSclsNm",
+    textAlign: "center",
+  },
+  {
+    name: "itemCd",
+    header: "itemCd",
+    textAlign: "center",
+    rowspan: ({ value, dataSourceArray, rowIndex, column }) => {
       let rowspan = 1;
       const prevData = dataSourceArray[rowIndex - 1];
-      if (prevData && prevData[column.name] === value) {
+      if (
+        prevData &&
+        !!prevData[column.name] &&
+        !!value &&
+        prevData[column.name] === value
+      ) {
         return rowspan;
       }
       let currentRowIndex = rowIndex + 1;
       while (
-        dataSourceArray[currentRowIndex] &&
+        !!dataSourceArray[currentRowIndex] &&
+        !!value &&
         dataSourceArray[currentRowIndex][column.name] === value
       ) {
         rowspan++;
         currentRowIndex++;
-        if (rowspan > 9) {
-          break;
-        }
       }
       return rowspan;
     },
+  },
+  {
+    name: "itemNm",
+    header: "itemNm",
+    textAlign: "center",
+  },
+  {
+    name: "uniEvntCd",
+    header: "uniEvntCd",
+    textAlign: "center",
+  },
+  {
+    name: "uniEvntSpNm",
+    header: "uniEvntSpNm",
+    textAlign: "center",
+  },
+  {
+    name: "evntTypeNm",
+    header: "evntTypeNm",
+    textAlign: "center",
+    render: (data) => {
+      return data?.value ? <div className="data-chip">{data?.value}</div> : "-";
+    },
+    // rowspan: ({ value, dataSourceArray, rowIndex, column }) => {
+    //   let rowspan = 1;
+    //   const prevData = dataSourceArray[rowIndex - 1];
+    //   if (prevData && prevData[column.name] === value) {
+    //     return rowspan;
+    //   }
+    //   let currentRowIndex = rowIndex + 1;
+    //   while (
+    //     dataSourceArray[currentRowIndex] &&
+    //     dataSourceArray[currentRowIndex][column.name] === value
+    //   ) {
+    //     rowspan++;
+    //     currentRowIndex++;
+    //   }
+    //   return rowspan;
+    // },
+  },
+  {
+    name: "evntGrpNm",
+    header: "evntGrpNm",
+    textAlign: "center",
+  },
+  {
+    name: "evntDurBeginDt",
+    header: "evntDurBeginDt",
+    textAlign: "center",
+  },
+  {
+    name: "evntDurEndDt",
+    header: "evntDurEndDt",
+    textAlign: "center",
+  },
+  {
+    name: "salesAmt",
+    header: "salesAmt",
+    textAlign: "center",
+  },
+  {
+    name: "salesQty",
+    header: "salesQty",
+    textAlign: "center",
+  },
+  {
+    name: "evntCst",
+    header: "evntCst",
+    textAlign: "center",
+  },
+  {
+    name: "evntSprc",
+    header: "evntSprc",
+    textAlign: "center",
+  },
+  {
+    name: "evntItemHdlStrCnt",
+    header: "evntItemHdlStrCnt",
+    textAlign: "center",
+  },
+  {
+    name: "saleStrCnt",
+    header: "saleStrCnt",
+    textAlign: "center",
+  },
+  {
+    name: "ratio",
+    header: "ratio",
+    textAlign: "center",
   },
 ];
 
@@ -95,7 +211,7 @@ const groupColumn = {
 };
 
 const RGDTable = () => {
-  const [selected, setSelected] = useState(options[2]);
+  const [selected, setSelected] = useState(options[0]);
 
   const handleChange = (value) => {
     setSelected(value);
@@ -103,20 +219,29 @@ const RGDTable = () => {
 
   const dataSource = useCallback(
     async ({ skip, sortInfo, limit = selected.value }) => {
-      return fetch(
-        "https://demos.reactdatagrid.io/api/v1/contacts" +
-          "?skip=" +
-          skip +
-          "&limit=" +
-          limit +
-          "&sortInfo=" +
-          JSON.stringify(sortInfo)
-      ).then((response) => {
-        const totalCount = response.headers.get("X-Total-Count");
-        return response.json().then((data) => {
-          return { data, count: parseInt(totalCount) };
+      console.log(skip, limit);
+
+      return new Promise((resolve, reject) => {
+        resolve({
+          count: 100,
+          data: data.data.content.slice(skip, limit + skip),
         });
       });
+
+      // const a = fetch(
+      //   "https://demos.reactdatagrid.io/api/v1/contacts" +
+      //     "?skip=" +
+      //     skip +
+      //     "&limit=" +
+      //     limit +
+      //     "&sortInfo=" +
+      //     JSON.stringify(sortInfo)
+      // ).then((response) => {
+      //   const totalCount = response.headers.get("X-Total-Count");
+      //   return response.json().then((data) => {
+      //     return { data, count: parseInt(totalCount) };
+      //   });
+      // });
     },
     [selected]
   );
@@ -142,7 +267,7 @@ const RGDTable = () => {
         pagination
         livePagination
         groupColumn={groupColumn}
-        defaultGroupBy={["pictureId"]}
+        defaultGroupBy={["salesDt"]}
         scrollThreshold={0.9}
         limit={selected.value}
       />
