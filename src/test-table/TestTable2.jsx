2d2ebd2ab566a11be0b84ce583415d11631c8aee
taphuong <taphuong@cmcglobal.vn>
2023-04-20 16:15:26 +0700

    update

--- src/test-table/TestTable2.jsx
+++ src/test-table/TestTable2.jsx
@@ -1,3 +1,12 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  useInfiniteQuery,
+} from "@tanstack/react-query";
+import MaterialReactTable, {
+  MRT_ShowHideColumnsButton,
+  MRT_ToggleGlobalFilterButton,
+} from "material-react-table";
 import React, {
   useCallback,
   useEffect,
@@ -5,39 +14,99 @@ import React, {
   useRef,
   useState,
 } from "react";
-import MaterialReactTable from "material-react-table";
-import { Typography } from "@mui/material";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useInfiniteQuery,
-} from "@tanstack/react-query";
+import mockData from "../rgd-table/mock.json";
+import "./style.scss";
+import { CSVLink } from "react-csv";
+import Select, { components } from "react-select";
 
 const columns = [
   {
-    accessorKey: "firstName",
-    header: "First Name",
+    header: "판매 코드",
+    accessorKey: "salesDt",
   },
   {
-    accessorKey: "lastName",
-    header: "Last Name",
+    header: "공급 코드",
+    accessorKey: "suppCd",
   },
   {
-    accessorKey: "address",
-    header: "Address",
+    header: "공급 이름",
+    accessorKey: "suppNm",
   },
   {
-    accessorKey: "state",
-    header: "State",
+    header: "구매 조건 코드",
+    accessorKey: "purchCondCd",
   },
   {
-    accessorKey: "phoneNumber",
-    header: "Phone Number",
+    header: "구매조건명",
+    accessorKey: "purchCondNm",
+  },
+  {
+    header: "아이템 코드",
+    accessorKey: "itemCd",
+  },
+  {
+    header: "상품명",
+    accessorKey: "itemNm",
+    size: 300,
+  },
+  {
+    header: "유니 이벤트 코드",
+    accessorKey: "uniEvntCd",
+  },
+  {
+    header: "유니 이벤트 이름",
+    accessorKey: "uniEvntSpNm",
+  },
+  {
+    header: "이벤트 유형 이름",
+    accessorKey: "evntTypeNm",
+    Cell: ({ cell }) => {
+      const value = cell.getValue();
+      console.log(value);
+      return value ? (
+        value === "2+1" ? (
+          <div className="data-chip-2">{value}</div>
+        ) : (
+          <div className="data-chip">{value}</div>
+        )
+      ) : (
+        "-"
+      );
+    },
+  },
+  {
+    header: "이벤트 그룹 이름",
+    accessorKey: "evntGrpNm",
+    size: 300,
+  },
+  {
+    header: "행사 시작일",
+    accessorKey: "evntDurBeginDt",
+  },
+  {
+    header: "이벤트 종료 날짜",
+    accessorKey: "evntDurEndDt",
+  },
+  {
+    header: "판매량",
+    accessorKey: "salesAmt",
+  },
+  {
+    header: "판매수량",
+    accessorKey: "salesQty",
+  },
+  {
+    header: "비율",
+    accessorKey: "ratio",
   },
 ];
 
 const fetchSize = 25;
-
+const options = [
+  { value: 20, label: "20 Record" },
+  { value: 50, label: "50 Record" },
+  { value: 100, label: "100 Record" },
+];
 const Example = () => {
   const tableContainerRef = useRef(null); //we can get access to the underlying TableContainer element and react to its scroll events
   const rowVirtualizerInstanceRef = useRef(null); //we can get access to the underlying Virtualizer instance and call its scrollToIndex method
@@ -45,26 +114,40 @@ const Example = () => {
   const [columnFilters, setColumnFilters] = useState([]);
   const [globalFilter, setGlobalFilter] = useState();
   const [sorting, setSorting] = useState([]);
-
+  const [pageSize, setPageSize] = useState(options[0]);
+  const handleChangePageSize = (value) => {
+    setPageSize(value);
+  };
+  
   const { data, fetchNextPage, isError, isFetching, isLoading } =
     useInfiniteQuery({
       queryKey: ["table-data", columnFilters, globalFilter, sorting],
       queryFn: async ({ pageParam = 0 }) => {
-        const url = new URL(
-          "/api/data",
-          process.env.NODE_ENV === "production"
-            ? "https://www.material-react-table.com"
-            : "https://www.material-react-table.com"
-        );
-        url.searchParams.set("start", `${pageParam * fetchSize}`);
-        url.searchParams.set("size", `${fetchSize}`);
-        url.searchParams.set("filters", JSON.stringify(columnFilters ?? []));
-        url.searchParams.set("globalFilter", globalFilter ?? "");
-        url.searchParams.set("sorting", JSON.stringify(sorting ?? []));
+        // const url = new URL(
+        //   "/api/data",
+        //   process.env.NODE_ENV === "production"
+        //     ? "https://www.material-react-table.com"
+        //     : "https://www.material-react-table.com"
+        // );
+        // url.searchParams.set("start", `${pageParam * fetchSize}`);
+        // url.searchParams.set("size", `${fetchSize}`);
+        // url.searchParams.set("filters", JSON.stringify(columnFilters ?? []));
+        // url.searchParams.set("globalFilter", globalFilter ?? "");
+        // url.searchParams.set("sorting", JSON.stringify(sorting ?? []));
 
-        const response = await fetch(url.href);
-        const json = await response.json();
-        return json;
+        // const response = await fetch(url.href);
+        // const json = await response.json();
+        // return json;
+
+        return new Promise((resolve, reject) => {
+          resolve({
+            meta: { totalRowCount: 100 },
+            data: [...mockData.data.content].slice(
+              pageParam * pageSize.value,
+              pageSize.value * (pageParam + 1)
+            ),
+          });
+        });
       },
       getNextPageParam: (_lastGroup, groups) => groups.length,
       keepPreviousData: true,
@@ -84,11 +167,10 @@ const Example = () => {
     (containerRefElement) => {
       if (containerRefElement) {
         const { scrollHeight, scrollTop, clientHeight } = containerRefElement;
-        //once the user has scrolled within 400px of the bottom of the table, fetch more data if we can
         if (
-          scrollHeight - scrollTop - clientHeight < 400 &&
-          !isFetching &&
-          totalFetched < totalDBRowCount
+          scrollHeight - scrollTop - clientHeight < 10 &&
+          !isFetching
+          // &&  totalFetched < totalDBRowCount
         ) {
           fetchNextPage();
         }
@@ -114,13 +196,13 @@ const Example = () => {
 
   return (
     <MaterialReactTable
+      enableGrouping
       columns={columns}
       data={flatData}
       enablePagination={false}
-      enableRowNumbers
-      enableRowVirtualization //optional, but recommended if it is likely going to be more than 100 rows
-      manualFiltering
-      manualSorting
+      enableRowVirtualization
+      //   manualFiltering
+      //   manualSorting
       muiTableContainerProps={{
         ref: tableContainerRef, //get access to the table container element
         sx: { maxHeight: "600px" }, //give the table a max height
@@ -128,22 +210,53 @@ const Example = () => {
           event //add an event listener to the table container element
         ) => fetchMoreOnBottomReached(event.target),
       }}
-      muiToolbarAlertBannerProps={
-        isError
-          ? {
-              color: "error",
-              children: "Error loading data",
-            }
-          : undefined
-      }
-      onColumnFiltersChange={setColumnFilters}
-      onGlobalFilterChange={setGlobalFilter}
-      onSortingChange={setSorting}
-      renderBottomToolbarCustomActions={() => (
-        <Typography>
-          Fetched {totalFetched} of {totalDBRowCount} total rows.
-        </Typography>
+      renderToolbarInternalActions={({ table }) => (
+        <>
+          <MRT_ShowHideColumnsButton table={table} />
+          <Select
+            className="react-select page-size-select"
+            value={pageSize}
+            options={options}
+            onChange={handleChangePageSize}
+          />
+          <div className="export-button react-btn">
+            <CSVLink
+              data={mockData.data.content}
+              headers={columns.map((item) => {
+                return { ...item, key: item.accessorKey, label: item.header };
+              })}
+              filename="data.csv"
+              target="_blank"
+            >
+              <img src="https://img.icons8.com/color/256/microsoft-excel-2019--v1.png" />
+              <label>다운로드</label>
+            </CSVLink>
+          </div>
+        </>
       )}
+      initialState={{ density: "compact" }}
+      enableColumnActions={false}
+      //   enableColumnDragging={false}
+      enableColumnOrdering={false}
+      //   enableTopToolbar={false}
+      enableBottomToolbar={false}
+      enableGlobalFilter={false}
+      //   muiToolbarAlertBannerProps={
+      //     isError
+      //       ? {
+      //           color: "error",
+      //           children: "Error loading data",
+      //         }
+      //       : undefined
+      //   }
+      //   onColumnFiltersChange={setColumnFilters}
+      //   onGlobalFilterChange={setGlobalFilter}
+      onSortingChange={setSorting}
+      //   renderBottomToolbarCustomActions={() => (
+      //     <Typography>
+      //       Fetched {totalFetched} of {totalDBRowCount} total rows.
+      //     </Typography>
+      //   )}
       state={{
         columnFilters,
         globalFilter,
